da400f08c7ee41d3dfd1d3166374ce36e2717414
刘生辉 <liushenghui@zbj.com>
2020-11-02 18:59:51 +0800

    初步实现

--- app/src/main/java/com/example/pintu/MainActivity.java
+++ app/src/main/java/com/example/pintu/MainActivity.java
@@ -2,19 +2,42 @@ package com.example.pintu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int screenWidth;
+    private static int screenHeight;
+
+    public static int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public static int getScreenHeight() {
+        return screenHeight;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(1);//全屏显示
-        getWindow().setFlags(1024,1024);
+       // requestWindowFeature(1);//全屏显示
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
-        
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+        setContentView(new MainView(this));
         //setContentView(R.layout.activity_main);
     }
+
+
 }
