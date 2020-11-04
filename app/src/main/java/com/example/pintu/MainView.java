0c27ef4aa15f7976c94b602aa92b358422704d55
刘生辉 <liushenghui@zbj.com>
2020-11-04 22:29:03 +0800

    前端文字居中，后端算法优化

--- app/src/main/java/com/example/pintu/MainView.java
+++ app/src/main/java/com/example/pintu/MainView.java
@@ -1,6 +1,7 @@
 package com.example.pintu;
 
-import android.app.AlertDialog;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.AssetManager;
@@ -9,41 +10,45 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 
-import com.example.pintu.util.MLog;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
 
 import static android.app.AlertDialog.*;
 
+@SuppressLint("ViewConstructor")
 public class MainView extends View {
-    private static final String TAG = MainView.class.getSimpleName();
+    private final String TITLE= "拼图大作战";
     private final Context context;
     private final int WIDTH = MainActivity2.getScreenWidth();//获取屏幕宽度
+    private final int HEIGHT = MainActivity2.getScreenWidth();
     private Bitmap back;//背景
-    private Paint paint;
-    private int tileWidth;
-    private int tileHeight;
+    private final Paint paint;
+    private int subWidth;
     private Bitmap[] bitmapTiles;//存储子图片
-    private int[][] dataTiles;//记录移动后的图片位置
-    private Board tilesBoard;
-    private  int COL;
-    private  int ROW;
-    private int[][] dir = {
+    private int[][] array;
+    private int[][] picPosition;//记录移动后的图片位置
+    private final int COL;
+    private final int ROW;
+    private boolean isSuccess;
+    int steps;
+    private final int[][] dir = {
             {-1, 0},//左
             {0, -1},//上
             {1, 0},//右
             {0, 1}//下
     };
-    private boolean isSuccess;
 
-    int steps = 0;
-
-    public MainView(Context context,int col) {
+    /**
+     * @param context context传入
+     * @param col     行数，难度
+     */
+    public MainView(Context context, int col) {
         super(context);
         this.context = context;
         this.COL = col;
@@ -52,7 +57,6 @@ public class MainView extends View {
         paint.setAntiAlias(true);
         init();
         startGame();
-        MLog.d(TAG, WIDTH + "," + WIDTH);
     }
 
     /**
@@ -64,17 +68,16 @@ public class MainView extends View {
         try {
             InputStream assetInputStream = assetManager.open("back.jpg");
             Bitmap bitmap = BitmapFactory.decodeStream(assetInputStream);
-            back = Bitmap.createScaledBitmap(bitmap, WIDTH, WIDTH, true);
+            back = Bitmap.createScaledBitmap(bitmap, WIDTH, HEIGHT, true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        tileWidth = back.getWidth() / COL;
-        tileHeight = back.getHeight() / ROW;
+        subWidth = back.getWidth() / COL;
         bitmapTiles = new Bitmap[COL * ROW];
         int idx = 0;
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
-                bitmapTiles[idx++] = Bitmap.createBitmap(back, j * tileWidth, i * tileHeight, tileWidth, tileHeight);
+                bitmapTiles[idx++] = Bitmap.createBitmap(back, j * subWidth, i * subWidth, subWidth, subWidth);
             }
         }
     }
@@ -83,8 +86,7 @@ public class MainView extends View {
      * 开始游戏
      */
     private void startGame() {
-        tilesBoard = new Board();
-        dataTiles = tilesBoard.createRandomBoard(ROW, COL);
+        picPosition = createRandomBoard(ROW, COL);
         isSuccess = false;
         steps = 0;
         invalidate();
@@ -95,13 +97,16 @@ public class MainView extends View {
         canvas.drawColor(Color.WHITE);
         paint.setTextSize(100);
         paint.setColor(Color.BLACK);
-        canvas.drawText("拼图大作战",500,300,paint);
+
+        @SuppressLint("DrawAllocation") Rect bounds = new Rect();
+        paint.getTextBounds(TITLE, 0, TITLE.length(), bounds);
+        canvas.drawText(TITLE, (getMeasuredWidth() >> 1) - (bounds.width() >> 1), 300, paint);
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
-                int idx = dataTiles[i][j];
+                int idx = picPosition[i][j];
                 if (idx == ROW * COL - 1 && !isSuccess)
                     continue;
-                canvas.drawBitmap(bitmapTiles[idx], j * tileWidth, i * tileHeight+500, paint);
+                canvas.drawBitmap(bitmapTiles[idx], j * subWidth, i * subWidth + 500, paint);
             }
         }
     }
@@ -109,42 +114,124 @@ public class MainView extends View {
     /**
      * 将屏幕上的点转换成,对应拼图块的索引
      *
-     * @param x
-     * @param y
-     * @return
+     * @param x 横坐标
+     * @param y 纵坐标
+     * @return 数组索引
      */
     private Point xyToIndex(int x, int y) {
-        int extraX = x % tileWidth > 0 ? 1 : 0;
-        int extraY = x % tileWidth > 0 ? 1 : 0;
-        int col = x / tileWidth + extraX;
-        int row = y / tileHeight + extraY;
+        int col = (x / subWidth) + (x % subWidth > 0 ? 1 : 0);
+        int row = (y / subWidth) + (y % subWidth > 0 ? 1 : 0);
 
         return new Point(col - 1, row - 1);
     }
 
+
+    /**
+     * @param arr 传入现图片排列顺序
+     * @return 是否成功
+     */
+    public boolean isSuccess(int[][] arr) {
+        int idx = 0;
+        for (int[] ints : arr) {
+            for (int j = 0; j < ints.length && idx < ROW * COL - 1; j++) {
+                if (ints[j] != idx) {
+                    return false;
+                }
+                idx++;
+            }
+        }
+        return true;
+    }
+
+    private void createBasicArray(int row, int col) {
+        array = new int[row][col];
+        int idx = 0;
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                array[i][j] = idx++;
+    }
+
+    /**
+     * 移动块的位置
+     * @param src 起始点
+     * @param dirId
+     * @return 新的位置，错误返回new Point(-1,-1);
+     */
+    public Point move(Point src, int dirId) {
+
+        int x = src.getX() + dir[dirId][0];
+        int y = src.getY() + dir[dirId][1];
+        if (!isPointIn(new Point(x,y))) {
+            return new Point(-1, -1);
+        }
+
+        int temp = array[y][x];
+        array[y][x] = array[src.getY()][src.getX()];
+        array[src.getY()][src.getX()] = temp;
+
+        return new Point(x, y);
+    }
+
+    /**
+     * 得到下一个可以移动的位置
+     *
+     * @param src
+     * @return
+     */
+    private Point getNextPoint(Point src) {
+        Random rd = new Random();
+        int idx = rd.nextInt(4);//产生0~3的随机数
+        Point newPoint = move(src, idx);
+        if (newPoint.getX() != -1 && newPoint.getY() != -1) {
+            return newPoint;
+        }
+        return getNextPoint(src);
+    }
+
+    /**
+     * 生成拼图数据
+     * @param row
+     * @param col
+     * @return
+     */
+    public int[][] createRandomBoard(int row, int col) {
+        createBasicArray(row, col);
+        int count = 0;
+        Point tempPoint = new Point(col - 1, row - 1);
+        Random rd = new Random();
+        int num = rd.nextInt(100) + 50;//产生50~149的随机数
+        while (count < num) {
+            tempPoint = getNextPoint(tempPoint);
+            count++;
+        }
+        return array;
+    }
+
+    public boolean isPointIn(Point point) {
+        return point.getX() >= 0 && point.getX() < ROW && point.getY() >= 0 && point.getY() < COL;
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            Point point = xyToIndex((int) event.getX(), (int) event.getY()-500);
+        if (event.getAction() == MotionEvent.ACTION_DOWN && event.getY() >= 500 && event.getY() <= 500 + back.getHeight()) {
+            Point point = xyToIndex((int) event.getX(), (int) event.getY() - 500);
 
-            for (int i = 0; i < dir.length; i++) {
-                int newX = point.getX() + dir[i][0];
-                int newY = point.getY() + dir[i][1];
+            for (int[] ints : dir) {
+                int newX = point.getX() + ints[0];
+                int newY = point.getY() + ints[1];
 
-                if (newX >= 0 && newX < COL && newY >= 0 && newY < ROW) {
-                    if (dataTiles[newY][newX] == COL * ROW - 1) {
+                if (isPointIn(new Point(newX,newY))) {
+                    if (picPosition[newY][newX] == COL * ROW - 1) {
                         steps++;
-                        int temp = dataTiles[point.getY()][point.getX()];
-                        dataTiles[point.getY()][point.getX()] = dataTiles[newY][newX];
-                        dataTiles[newY][newX] = temp;
+                        int temp = picPosition[point.getY()][point.getX()];
+                        picPosition[point.getY()][point.getX()] = picPosition[newY][newX];
+                        picPosition[newY][newX] = temp;
                         invalidate();
-                        if (tilesBoard.isSuccess(dataTiles)) {
+                        if (isSuccess(picPosition)) {
                             isSuccess = true;
                             invalidate();
-                            String successText = String.format("恭喜你拼图成功，移动了"+steps+"次");
-                            new
-
-                                    Builder(context)
+                            String successText = String.format("恭喜你拼图成功，一共移动了" + steps + "次");
+                            new Builder(context)
                                     .setTitle("拼图成功")
                                     .setCancelable(false)
                                     .setMessage(successText)
@@ -168,16 +255,5 @@ public class MainView extends View {
             }
         }
         return true;
-    }
-
-    private void printArray(int[][] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                sb.append(arr[i][j] + ",");
-            }
-            sb.append("\n");
-        }
-        MLog.d(TAG, sb.toString());
     }
 }
